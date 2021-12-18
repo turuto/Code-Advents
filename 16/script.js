df1dfc59ce03dad8f2325309bcd986995f360af7
Alberto García <turuto@gmail.com>
2021-12-18 07:59:49 +0100

    challenge 16

--- 16/script.js
+++ 16/script.js
@@ -1,18 +1,33 @@
 function decodeNumbers(symbols) {
-    // ¡No olvides compartir tu solución en redes!
+    const DICTIONARY = {
+        '.': 1,
+        ',': 5,
+        ':': 10,
+        ';': 50,
+        '!': 100,
+    };
+    const tokens = symbols.split('');
 
-    return false;
+    const add = (accumulator, item, index) => {
+        let value = DICTIONARY[item];
+        let nextItem = tokens[index + 1];
+        let nextValue = DICTIONARY[nextItem];
+        let sign = ((nextValue - value) > 0) ? -1 : 1;
+        return accumulator + value * sign;
+    }
+
+    return tokens.reduce(add, 0);
 }
 
-console.log(decodeNumbers('...')); // 3
-// console.log(decodeNumbers('.,')); // 4 (5 - 1)
-// console.log(decodeNumbers(',.')); // 6 (5 + 1)
-// console.log(decodeNumbers(',...')); // 8 (5 + 3)
-// console.log(decodeNumbers('.........!')); // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
-// console.log(decodeNumbers('.;')); // 49 (50 - 1)
-// console.log(decodeNumbers('..,')); // 5 (-1 + 1 + 5)
-// console.log(decodeNumbers('..,!')); // 95 (1 - 1 - 5 + 100)
-// console.log(decodeNumbers('.;!')); // 49 (-1 -50 + 100)
-// console.log(decodeNumbers('!!!')); // 300
-// console.log(decodeNumbers(';!')); // 50
-// console.log(decodeNumbers(';.W')); // NaN
+// console.log(decodeNumbers('...')); // 3
+console.log(decodeNumbers('.,')); // 4 (5 - 1)
+console.log(decodeNumbers(',.')); // 6 (5 + 1)
+console.log(decodeNumbers(',...')); // 8 (5 + 3)
+console.log(decodeNumbers('.........!')); // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+console.log(decodeNumbers('.;')); // 49 (50 - 1)
+console.log(decodeNumbers('..,')); // 5 (-1 + 1 + 5)
+console.log(decodeNumbers('..,!')); // 95 (1 - 1 - 5 + 100)
+console.log(decodeNumbers('.;!')); // 49 (-1 -50 + 100)
+console.log(decodeNumbers('!!!')); // 300
+console.log(decodeNumbers(';!')); // 50
+console.log(decodeNumbers(';.W')); // NaN
