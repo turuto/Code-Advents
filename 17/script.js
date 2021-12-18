57f3c28f7aeeb78f7cc988e310856ffa16f3621a
Alberto García <turuto@gmail.com>
2021-12-18 07:33:27 +0100

    base 16 y 17

---
+++ 17/script.js
@@ -0,0 +1,18 @@
+function decodeNumbers(symbols) {
+    // ¡No olvides compartir tu solución en redes!
+
+    return 'jose lios';
+}
+
+console.log(decodeNumbers('...')); // 3
+// console.log(decodeNumbers('.,')); // 4 (5 - 1)
+// console.log(decodeNumbers(',.')); // 6 (5 + 1)
+// console.log(decodeNumbers(',...')); // 8 (5 + 3)
+// console.log(decodeNumbers('.........!')); // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+// console.log(decodeNumbers('.;')); // 49 (50 - 1)
+// console.log(decodeNumbers('..,')); // 5 (-1 + 1 + 5)
+// console.log(decodeNumbers('..,!')); // 95 (1 - 1 - 5 + 100)
+// console.log(decodeNumbers('.;!')); // 49 (-1 -50 + 100)
+// console.log(decodeNumbers('!!!')); // 300
+// console.log(decodeNumbers(';!')); // 50
+// console.log(decodeNumbers(';.W')); // NaN
