cd4ee09c677e7f4a35e7ab1649c8510f8d28fdae
Alberto García <turuto@gmail.com>
2021-12-19 08:45:20 +0100

    reto 17

--- 17/script.js
+++ 17/script.js
@@ -1,18 +1,35 @@
-function decodeNumbers(symbols) {
-    // ¡No olvides compartir tu solución en redes!
+function countPackages(carriers, carrierID) {
+    const worker = carriers.filter(item => item[0] === carrierID)[0];
+    let capacity = worker[1];
 
-    return 'jose lios';
+    if (worker[2].length > 0) {
+        worker[2].forEach(subordinate => {
+            capacity += countPackages(carriers, subordinate);
+        })
+    }
+
+    return capacity;
 }
 
-console.log(decodeNumbers('...')); // 3
-// console.log(decodeNumbers('.,')); // 4 (5 - 1)
-// console.log(decodeNumbers(',.')); // 6 (5 + 1)
-// console.log(decodeNumbers(',...')); // 8 (5 + 3)
-// console.log(decodeNumbers('.........!')); // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
-// console.log(decodeNumbers('.;')); // 49 (50 - 1)
-// console.log(decodeNumbers('..,')); // 5 (-1 + 1 + 5)
-// console.log(decodeNumbers('..,!')); // 95 (1 - 1 - 5 + 100)
-// console.log(decodeNumbers('.;!')); // 49 (-1 -50 + 100)
-// console.log(decodeNumbers('!!!')); // 300
-// console.log(decodeNumbers(';!')); // 50
-// console.log(decodeNumbers(';.W')); // NaN
+
+const carriers = [
+    ['dapelu', 5, ['midu', 'jelowing']],
+    ['midu', 2, []],
+    ['jelowing', 2, []]
+]
+
+console.log(countPackages(carriers, 'dapelu')); // 9
+// 5 de dapelu, 2 de midu y 2 de jelowing = 9
+
+const carriers2 = [
+    ['lolivier', 8, ['camila', 'jesuspoleo']],
+    ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+    ['jesuspoleo', 4, []],
+    ['sergiomartinez', 4, []],
+    ['conchaasensio', 3, ['facundocapua', 'faviola']],
+    ['facundocapua', 2, []],
+    ['faviola', 1, []]
+]
+
+console.log(countPackages(carriers2, 'camila')); // 15
+  // 5 de camila, 4 de sergiomartinez, 3 de conchaasensio, 2 de facundocapua y 1 de faviola = 15
