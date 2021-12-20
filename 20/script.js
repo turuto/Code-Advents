bec7c9086e0bb28ea3b715394825bd07260e0cd6
Alberto García <turuto@gmail.com>
2021-12-20 17:35:18 +0100

    base 18-20

---
+++ 20/script.js
@@ -0,0 +1,10 @@
+function  function pangram(letter) {
+    // ¡No olvides compartir tu solución en redes!
+    return false
+}
+
+console.log(pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho')); // true
+console.log(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibe!')); // true
+
+console.log(pangram('Esto es una frase larga pero no tiene todas las letras del abecedario')); // false
+console.log(pangram('De la a a la z, nos faltan letras')); // false
