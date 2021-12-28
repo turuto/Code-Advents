0f342b3f835400d132744ed2414d232df34e637c
Alberto García <turuto@gmail.com>
2021-12-28 10:53:25 +0100

    reto 25

--- 25/script.js
+++ 25/script.js
@@ -1,6 +1,42 @@
 function canMouseEat(direction, game) {
-    // ¡Gracias por jugar a AdventJS 2021! 🤗
-    // ¡Nos vemos el año que viene! 👋
-    // Por favor, comparte en las redes qué te ha parecido! 🚀
-    return false
+    const MOUSE = 'm',
+        FOOD = '*';
+    const columns = game[0].length,
+        MOVEMENTS = {
+            up: -columns,
+            down: columns,
+            left: -1,
+            right: 1,
+        }
+
+    const flattenedGame = game.flat();
+
+    const mousePos = flattenedGame.indexOf(MOUSE);
+    const newPos = mousePos + MOVEMENTS[direction];
+
+    return flattenedGame[newPos] === FOOD;
 }
+
+const room = [
+    [' ', ' ', ' '],
+    [' ', ' ', 'm'],
+    [' ', ' ', '*']
+]
+// console.log(room);
+// console.log(canMouseEat('up', room));   // false
+// console.log(canMouseEat('down', room));   // true
+// console.log(canMouseEat('right', room));   // false
+// console.log(canMouseEat('left', room));   // false
+
+const room2 = [
+    ['*', ' ', ' ', ' '],
+    [' ', 'm', '*', ' '],
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', '*']
+]
+
+console.log(room2);
+console.log(canMouseEat('up', room2));   // false
+console.log(canMouseEat('down', room2));   // false
+console.log(canMouseEat('right', room2));   // true
+console.log(canMouseEat('left', room2));   // false
