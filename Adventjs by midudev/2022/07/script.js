03eb847294d30b2fd8b4a562614058c2e0758974
Alberto García <turuto@gmail.com>
2022-12-07 20:34:20 +0100

    07

--- Adventjs by midudev/2022/07/script.js
+++ Adventjs by midudev/2022/07/script.js
@@ -1,27 +1,10 @@
 function getGiftsToRefill(a1, a2, a3) {
-    const allWarehouses = a1.concat(a2).concat(a3);
-    const singleProds =[... new Set(allWarehouses)];
-    const objectsToRefill =[];
-    const counts =singleProds.map(product => {
-        let count = 0;
-        let isInWare1 = a1.includes(product);
-        let isInWare2 = a2.includes(product);
-        let isInWare3 = a3.includes(product);
-        if (isInWare1) {
-            count ++;
-        }
-        if (isInWare2) {
-            count ++;
-        }
-        if (isInWare3) {
-            count ++;
-        }
-        if (count === 1) {
-            objectsToRefill.push(product)
-        }
+    const stores = [...new Set([...a1, ...a2, ...a3])]
+
+    return stores.filter((gift) => {
+      return a1.includes(gift) + a2.includes(gift) + a3.includes(gift) === 1
     })
-    return objectsToRefill;
-}
+  }
 
 const a1 = ['bici', 'coche', 'bici', 'bici']
 const a2 = ['coche', 'bici', 'muñeca', 'coche']
