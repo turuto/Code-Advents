8a012ceb633c55ba65a469ebd6a0ba11543f470f
Alberto García <turuto@gmail.com>
2022-12-08 20:55:17 +0100

    08

---
+++ Adventjs by midudev/2022/08/script.js
@@ -0,0 +1,17 @@
+function checkPart(part) {
+    const isPalindrome = function (txt) {
+        const reversed = txt.split('').reverse().join('');
+        return reversed === txt;
+    }
+    if (isPalindrome(part)) return true;
+    for (let i=0; i<part.length; i++) {
+        const reduced = part.substring(0, i) + part.substring(i+1);
+        if (isPalindrome(reduced)) return true;
+    }
+    return false
+}
+checkPart('hola');
+console.log('sdkjsdlkjds');
+// console.log(checkPart("uwu")); // true
+// console.log(checkPart("miidim")); // true
+// console.log(checkPart("midu")); // false
