343f3c601cfcf4eaf4b5a4cef9d35c814219eddd
Alberto García Ariza <turuto@gmail.com>
2023-10-16 00:18:02 +0200

    challenge 13

--- Adventjs by midudev/2022/13/script.js
+++ Adventjs by midudev/2022/13/script.js
@@ -1,3 +1,17 @@
-function getFilesToBackup(lastBackup, changes) {
-  return []
+export default function getFilesToBackup(lastBackup, changes) {
+  const unsavedChanges = changes.filter((changes) => changes[1] > lastBackup);
+  const filesToSave = unsavedChanges.map((item) => item[0]);
+  const sortedIds = filesToSave.sort((a, b) => a - b);
+  const flatedIds = [...new Set(sortedIds)];
+  return flatedIds;
 }
+
+console.log(
+  getFilesToBackup(1546300800, [
+    [1, 1546300800],
+    [2, 1546300800],
+    [1, 1546300900],
+    [1, 1546301000],
+    [3, 1546301100],
+  ])
+);
