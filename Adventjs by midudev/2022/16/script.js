250ddd56d7fbbdd481af22704b73134e782a7df0
Alberto García Ariza <turuto@gmail.com>
2023-10-23 15:49:07 +0200

    challenge 16

--- Adventjs by midudev/2022/16/script.js
+++ Adventjs by midudev/2022/16/script.js
@@ -13,7 +13,7 @@ export default function fixLetter(letter) {
 
     // Quitar espacios antes de coma o punto
     const removeSpaceBeforePunctuation = (str) => {
-        return str.replace(/\s+([,.])/g, '$1');
+        return str.replace(/\s+([,.?])/g, '$1');
     };
     result = removeSpaceBeforePunctuation(result);
 
@@ -21,10 +21,45 @@ export default function fixLetter(letter) {
     const removeMultipleQuestionMarks = (str) => str.replace(/\?+/g, '?');
     result = removeMultipleQuestionMarks(result);
     // La primera letra de cada oración debe estar en mayúscula
+    const beginSentenceWithUppercase = (str) => {
+        let fixedStr = '';
+
+        const firstChar = str.charAt(0).toUpperCase();
+        fixedStr = firstChar + str.slice(1);
+
+        const endOfSentencePattern = /[.!?]\s+[A-Za-z]/g;
+        const occurrences = fixedStr.match(endOfSentencePattern);
+        if (occurrences) {
+            const fixedOccurrences = occurrences.map((item) => {
+                const lastChar = item.substr(-1);
+                const remainingText = item.slice(0, -1);
+                return remainingText + lastChar.toUpperCase();
+            });
+
+            fixedOccurrences.forEach((fixedItem, index) => {
+                fixedStr = fixedStr.replace(occurrences[index], fixedItem);
+            });
+        }
+
+        return fixedStr;
+    };
+
+    result = beginSentenceWithUppercase(result);
+
     // Poner en mayúscula la palabra "Santa Claus" si aparece en la carta
+    const writeSantaClausProperly = (str) => {
+        return str.replace(/santa claus/gi, 'Santa Claus');
+    };
+    result = writeSantaClausProperly(result);
     // Poner un punto al final de la frase si no tiene puntuación
+    const addPeriodAtTheEndIfNeeded = (str) => {
+        const regex = /[.?!]\s*$/;
+        const punctuationNeeded = regex.test(str);
+        return punctuationNeeded ? str : str + '.';
+    };
+    result = addPeriodAtTheEndIfNeeded(result);
     return result;
 }
 
 // Example usage
-console.log(fixLetter('   HOLA  que   tal   '));
+console.log(fixLetter('   HOLA  que   tal.    como?   exact!     '));
--- Adventjs by midudev/2022/16/script.test.js
+++ Adventjs by midudev/2022/16/script.test.js
@@ -6,72 +6,90 @@ test('Test #1 - Retorna un String', () => {
 
 test('Test #A - Eliminar espacios al inicio y al final', () => {
     expect(fixLetter(`    hello, how are you?    `)).toBe(
-        'hello, how are you?'
+        'Hello, how are you?'
     );
 });
 
 test('Test #B - Eliminar múltiples espacios en blanco y dejar sólo uno', () => {
     expect(fixLetter(`    hello,   how    are you?    `)).toBe(
-        'hello, how are you?'
+        'Hello, how are you?'
     );
 });
 
 test('Test #C - Dejar un espacio después de cada coma', () => {
     expect(fixLetter(`    hello,how,    are you?    `)).toBe(
-        'hello, how, are you?'
+        'Hello, how, are you?'
     );
 });
 
 test('Test #D - Quitar espacios antes de coma o punto', () => {
     expect(fixLetter(`    hello ,how .     are you?    `)).toBe(
-        'hello, how. are you?'
+        'Hello, how. Are you?'
     );
 });
 
 test('Test #E - Las preguntas sólo deben terminar con un signo de interrogación', () => {
     expect(fixLetter(`    hello ,how .     are you??    `)).toBe(
-        'hello, how. are you?'
+        'Hello, how. Are you?'
     );
 });
 
-// test('Test #2 - fixLetter(` hello,  how are you??     do you know if santa claus exists?  i really hope he does!  bye  `)', () => {
-//     expect(
-//         fixLetter(
-//             ` hello,how are you??     do you know if santa claus exists?  i really hope he does!  bye  `
-//         )
-//     ).toBe(
-//         'Hello, how are you? Do you know if Santa Claus exists? I really hope he does! Bye.'
-//     );
-// });
+test('Test #E2 - Las preguntas sólo deben tener un signo de puntuación', () => {
+    expect(fixLetter(`    hello ,how .     are you??    `)).toBe(
+        'Hello, how. Are you?'
+    );
+});
 
-// test(`Test #3 - fixLetter("  Hi Santa claus. I'm a girl from Barcelona , Spain . please, send me a bike.  Is it possible?")`, () => {
-//     expect(
-//         fixLetter(
-//             "  Hi Santa claus. I'm a girl from Barcelona , Spain . please, send me a bike.  Is it possible?"
-//         )
-//     ).toBe(
-//         "Hi Santa Claus. I'm a girl from Barcelona, Spain. Please, send me a bike. Is it possible?"
-//     );
-// });
+test('Test #F - Poner en mayúscula la palabra "Santa Claus" si aparece en la carta', () => {
+    expect(
+        fixLetter(` hello ,santa Claus.  SAnta CLAUS santa claus   are you?`)
+    ).toBe('Hello, Santa Claus. Santa Claus Santa Claus are you?');
+});
 
-// test(`Test #4 - fixLetter("  hi    santa    claus ")`, () => {
-//     expect(fixLetter('  hi    santa    claus ')).toBe('Hi Santa Claus.');
-// });
+test('Test #g - Poner un punto al final de la frase si no tiene puntuación', () => {
+    expect(
+        fixLetter(` hello ,santa Claus.  SAnta CLAUS santa claus   are you`)
+    ).toBe('Hello, Santa Claus. Santa Claus Santa Claus are you.');
+});
 
-// test(`Test #5 - fixLetter("  hi    santa    claus . santa claus is the best  ")`, () => {
-//     expect(
-//         fixLetter('  hi    santa    claus . santa claus is the best  ')
-//     ).toBe('Hi Santa Claus. Santa Claus is the best.');
-// });
+test('Test #2 - fixLetter(` hello,  how are you??     do you know if santa claus exists?  i really hope he does!  bye  `)', () => {
+    expect(
+        fixLetter(
+            ` hello,how are you??     do you know if santa claus exists?  i really hope he does!  bye  `
+        )
+    ).toBe(
+        'Hello, how are you? Do you know if Santa Claus exists? I really hope he does! Bye.'
+    );
+});
 
-// test(`Test #6 - fixLetter("  hi    santa    claus ??")`, () => {
-//     expect(fixLetter('  hi    santa    claus . are you there???')).toBe(
-//         'Hi Santa Claus. Are you there?'
-//     );
-// });
+test(`Test #3 - fixLetter("  Hi Santa claus. I'm a girl from Barcelona , Spain . please, send me a bike.  Is it possible?")`, () => {
+    expect(
+        fixLetter(
+            "  Hi Santa claus. I'm a girl from Barcelona , Spain . please, send me a bike.  Is it possible?"
+        )
+    ).toBe(
+        "Hi Santa Claus. I'm a girl from Barcelona, Spain. Please, send me a bike. Is it possible?"
+    );
+});
 
-// test(`Test #7 - fixLetter("Hey santa Claus .  I want a bike.   I want a videogame! ")`, () => {
-//     expect(
-//         fixLetter('Hey santa Claus .  I want a bike.   I want a videogame! ')
-//     ).toBe('Hey Santa Claus. I want a bike. I want a videogame!');
-// });
+test(`Test #4 - fixLetter("  hi    santa    claus ")`, () => {
+    expect(fixLetter('  hi    santa    claus ')).toBe('Hi Santa Claus.');
+});
+
+test(`Test #5 - fixLetter("  hi    santa    claus . santa claus is the best  ")`, () => {
+    expect(
+        fixLetter('  hi    santa    claus . santa claus is the best  ')
+    ).toBe('Hi Santa Claus. Santa Claus is the best.');
+});
+
+test(`Test #6 - fixLetter("  hi    santa    claus ??")`, () => {
+    expect(fixLetter('  hi    santa    claus . are you there???')).toBe(
+        'Hi Santa Claus. Are you there?'
+    );
+});
+
+test(`Test #7 - fixLetter("Hey santa Claus .  I want a bike.   I want a videogame! ")`, () => {
+    expect(
+        fixLetter('Hey santa Claus .  I want a bike.   I want a videogame! ')
+    ).toBe('Hey Santa Claus. I want a bike. I want a videogame!');
+});
