c154285e1978af7246167418f0b64457aa9a5049
Alberto García Ariza <turuto@gmail.com>
2023-10-25 07:17:58 +0200

    challenge 19 solved

--- Adventjs by midudev/2022/19/script.js
+++ Adventjs by midudev/2022/19/script.js
@@ -1,3 +1,21 @@
 export default function sortToys(toys, positions) {
-    return [];
+    const calculateRealPositions = (positions) => {
+        const minPosition = Math.min(...positions);
+        const realPositions = positions.map((item) => item - minPosition);
+        return realPositions;
+    };
+
+    const realPositions = calculateRealPositions(positions);
+    const sortedArray = [];
+    toys.forEach((toy, index) => {
+        const newIndex = realPositions[index];
+        sortedArray[newIndex] = toy;
+    });
+    return sortedArray;
 }
+
+const moreToys = ['pc', 'xbox', 'ps4', 'switch', 'nintendo'];
+const morePositions = [8, 6, 5, 7, 9];
+
+console.log(sortToys(moreToys, morePositions));
+// ['ps4', 'xbox', 'switch', 'pc', 'nintendo']
